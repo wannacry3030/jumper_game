d3fadcf9dd18ed92cf4423c8a45ee3a8ee8c5707
wannacry3030 <wannacry.bat@proton.me>
2023-06-03 14:23:04 -0300

    update plataforma.py

--- plataforma.py
+++ plataforma.py
@@ -71,9 +71,10 @@ class Player(pygame.sprite.Sprite):
 class platform(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
-        self.surf.fill((255, 0, 0))
-        self.rect = self.surf.get_rect(center=(WIDTH/2, HEIGHT - 10))
+        self.surf = pygame.Surface((random.randint(50, 100), 12))
+        self.surf.fill((0, 255, 0))
+        self.rect = self.surf.get_rect(
+            center=(random.randint(0, WIDTH-10), random.randint(0, HEIGHT-30)))
 
     def move(self):
         pass
