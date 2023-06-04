6a7d3e9629774916a8ffa08c8d095e85eeb00f15
wannacry3030 <wannacry.bat@proton.me>
2023-06-04 18:00:53 -0300

    update jumperteste2.py

--- jumperteste2.py
+++ jumperteste2.py
@@ -3,11 +3,12 @@ from pygame.locals import *
 import sys
 import random
 import time
+import pygame.mixer
 
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("music.mp3")
-pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.set_volume(0.2)
 pygame.mixer.music.play(-1)
 
 vec = pygame.math.Vector2  # 2 for two dimensional
@@ -29,7 +30,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.surf_left = pygame.image.load("snowman_left.png")
         self.surf_right = pygame.image.load("snowman_right.png")
-        self.surf = self.surf_right
+        self.surf = self.surf_right  # Inicialmente, carrega a imagem voltada para a direita
         self.rect = self.surf.get_rect()
 
         self.pos = vec((10, 360))
@@ -37,7 +38,7 @@ class Player(pygame.sprite.Sprite):
         self.acc = vec(0, 0)
         self.jumping = False
         self.score = 0
-        self.direction = "right"
+        self.direction = "right"  # Inicialmente, o personagem está voltado para a direita
 
     def move(self):
         self.acc = vec(0, 0.5)
@@ -46,9 +47,11 @@ class Player(pygame.sprite.Sprite):
 
         if pressed_keys[K_LEFT]:
             self.acc.x = -ACC
+            # Altera a direção para "left" quando a tecla "left" é pressionada
             self.direction = "left"
         if pressed_keys[K_RIGHT]:
             self.acc.x = ACC
+            # Altera a direção para "right" quando a tecla "right" é pressionada
             self.direction = "right"
 
         self.acc.x += self.vel.x * FRIC
@@ -94,27 +97,46 @@ class Player(pygame.sprite.Sprite):
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, width=43, height=28):
         super().__init__()
-        self.surf_left = pygame.image.load("enemy_left.png")
-        self.surf_right = pygame.image.load("enemy_right.png")
-        self.surf = self.surf_right
+
+        self.original_image = pygame.image.load("bat.png")
+        self.surf = pygame.transform.scale(
+            self.original_image, (width, height))
         self.rect = self.surf.get_rect()
 
-        self.pos = vec((random.randint(0, WIDTH), -100))
-        self.vel = vec(0, 0)
+        # Set the initial x position on either the left or right side of the screen
+        if random.choice([True, False]):
+            self.rect.centerx = random.randint(0, WIDTH // 2 - width // 2)
+        else:
+            self.rect.centerx = random.randint(WIDTH // 2 + width // 2, WIDTH)
+
+        self.rect.centery = random.randint(0, HEIGHT - 30)
+
+        self.point = True
+        self.moving = True
+        self.speed = random.randint(1, 3)
+
+        if random.choice([True, False]):
+            self.speed *= -1
 
     def move(self):
-        self.vel.y = 5
-        self.pos += self.vel
-        if self.pos.y > HEIGHT:
-            self.pos.y = random.randint(-100, -10)
-            self.pos.x = random.randint(0, WIDTH)
+        hits = self.rect.colliderect(P1.rect)
+        if self.moving:
+            self.rect.move_ip(self.speed, 0)
+            if hits:
+                P1.pos.x += self.speed
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
+            if self.rect.left > WIDTH or self.rect.right < 0:
+                self.kill()
 
-        self.rect.midbottom = self.pos
-
-    def draw(self):
-        displaysurface.blit(self.surf, self.rect)
+    def update_image(self):
+        # Scale the image using the original dimensions
+        self.surf = pygame.transform.scale(
+            self.original_image, (self.rect.width, self.rect.height))
 
 
 class Coin(pygame.sprite.Sprite):
@@ -151,7 +173,7 @@ class Platform(pygame.sprite.Sprite):
         if self.speed == 0:
             self.moving = False
 
-    def generate_coin(self):
+    def generateCoin(self):
         if self.speed == 0:
             coins.add(Coin((self.rect.centerx, self.rect.centery - 50)))
 
@@ -183,15 +205,15 @@ def plat_gen():
     while len(platforms) < 6:
         width = random.randrange(50, 100)
         p = Platform()
-        c = True
+        C = True
 
-        while c:
+        while C:
             p = Platform()
             p.rect.center = (random.randrange(0, WIDTH - width),
                              random.randrange(-50, 0))
-            c = check(p, platforms)
+            C = check(p, platforms)
 
-        p.generate_coin()
+        p.generateCoin()
         platforms.add(p)
         all_sprites.add(p)
 
@@ -199,6 +221,7 @@ def plat_gen():
 all_sprites = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
 coins = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
 
 PT1 = Platform(450, 80)
 
@@ -212,23 +235,26 @@ E1 = Enemy()
 
 all_sprites.add(PT1)
 all_sprites.add(P1)
+all_sprites.add(E1)
 platforms.add(PT1)
 
 for x in range(random.randint(4, 5)):
-    c = True
+    C = True
     pl = Platform()
-    while c:
+    while C:
         pl = Platform()
-        c = check(pl, platforms)
-    pl.generate_coin()
+        C = check(pl, platforms)
+    pl.generateCoin()
     platforms.add(pl)
     all_sprites.add(pl)
 
-enemy_timer = pygame.time.get_ticks() + 5000  # Initial delay of 5 seconds
+# Tempo inicial para o próximo spawn de inimigo
+enemy_spawn_time = pygame.time.get_ticks() + random.randint(2000, 5000)
+
 
 while True:
     P1.update()
-
+    E1.update()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -240,61 +266,49 @@ while True:
             if event.key == pygame.K_SPACE:
                 P1.cancel_jump()
 
-    if pygame.time.get_ticks() >= enemy_timer:
-        e = Enemy()
-        all_sprites.add(e)
-        enemy_timer += 5000
+    if P1.rect.top > HEIGHT:
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(1)
+        displaysurface.fill((255, 0, 0))
+        pygame.display.update()
+        time.sleep(1)
+        pygame.quit()
+        sys.exit()
+
+    if P1.rect.top <= HEIGHT / 3:
+        P1.pos.y += abs(P1.vel.y)
+        for plat in platforms:
+            plat.rect.y += abs(P1.vel.y)
+            if plat.rect.top >= HEIGHT:
+                plat.kill()
+
+        for coin in coins:
+            coin.rect.y += abs(P1.vel.y)
+            if coin.rect.top >= HEIGHT:
+                coin.kill()
+
+    if pygame.time.get_ticks() > enemy_spawn_time:
+        enemy_spawn_time = pygame.time.get_ticks() + random.randint(2000, 5000)
+        E = Enemy()
+        all_sprites.add(E)
+        enemies.add(E)  # Adicionar o objeto Enemy ao grupo de inimigos
+
+    plat_gen()
+    displaysurface.blit(background, (0, 0))
+    f = pygame.font.SysFont("Verdana", 20)
+    g = f.render(str(P1.score), True, (123, 255, 0))
+    displaysurface.blit(g, (WIDTH/2, 10))
 
     for entity in all_sprites:
-        displaysurface.blit(background, (0, 0))
-        entity.move()
         displaysurface.blit(entity.surf, entity.rect)
+        entity.move()
 
-    if P1.jumping:
-        P1.surf = pygame.image.load("snowman_jump.png")
-    else:
-        if P1.direction == "left":
-            P1.surf = pygame.image.load("snowman_left.png")
-        elif P1.direction == "right":
-            P1.surf = pygame.image.load("snowman_right.png")
+    for coin in coins:
+        displaysurface.blit(coin.image, coin.rect)
+        coin.update()
 
-    if P1.vel.y != 0:
-        P1.jumping = True
-    platforms.update()
-
-    coins.update()
-    coins.draw(displaysurface)
-
-    if P1.score >= 20:
-        pygame.mixer.music.pause()
-
-        font = pygame.font.Font("freesansbold.ttf", 20)
-        text = font.render("Você Ganhou", True, (255, 255, 255), (0, 0, 0))
-        textRect = text.get_rect()
-        textRect.center = (WIDTH // 2, HEIGHT // 2)
-        displaysurface.blit(text, textRect)
-
-        pygame.display.update()
-        time.sleep(2)
-        pygame.quit()
-        sys.exit()
-
-    if pygame.sprite.spritecollideany(P1, E1):
-        pygame.mixer.music.pause()
-
-        P1.kill()
-        font = pygame.font.Font("freesansbold.ttf", 20)
-        text = font.render("Game Over", True, (255, 255, 255), (0, 0, 0))
-        textRect = text.get_rect()
-        textRect.center = (WIDTH // 2, HEIGHT // 2)
-        displaysurface.blit(text, textRect)
-
-        pygame.display.update()
-        time.sleep(2)
-        pygame.quit()
-        sys.exit()
-
-    displaysurface.blit(P1.surf, P1.rect)
+    P1.draw()
 
     pygame.display.update()
     FramePerSec.tick(FPS)
